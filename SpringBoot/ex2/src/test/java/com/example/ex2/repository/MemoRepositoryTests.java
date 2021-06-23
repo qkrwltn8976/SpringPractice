81357b45b099e0050dd4e0a0085ee40804593e08
unknown <qkrwltn8976@dongguk.edu>
2021-06-23 16:56:37 +0900

    Update MemoRepositoryTests.java

--- SpringBoot/ex2/src/test/java/com/example/ex2/repository/MemoRepositoryTests.java
+++ SpringBoot/ex2/src/test/java/com/example/ex2/repository/MemoRepositoryTests.java
@@ -4,6 +4,10 @@ import com.example.ex2.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -71,5 +75,17 @@ public class MemoRepositoryTests {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPaging() {
+
+        Sort sort1 = Sort.by("mno").descending();
+
+        Pageable pageable = PageRequest.of(0, 10, sort1);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println("===========================");
     }
 }
