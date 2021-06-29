036d8fb2c5f1cfed407ebc762ce113dd5bcbe8cf
unknown <qkrwltn8976@dongguk.edu>
2021-06-29 16:53:18 +0900

    게시판

--- SpringMVC/ex2/src/test/java/org/zerock/service/BoardServiceTest.java
+++ SpringMVC/ex2/src/test/java/org/zerock/service/BoardServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -20,5 +21,24 @@ public class BoardServiceTest {
 	public void testPrint() {
 		
 		log.info(service);
+	}
+	
+	@Test
+	public void testGetList() {
+		
+		service.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testRegister() {
+		
+		BoardVO vo = new BoardVO();
+		vo.setTitle("Test 테스트");
+		vo.setContent("Content 테스트");
+		vo.setWriter("tester");
+		
+		long bno = service.register(vo);
+		
+		log.info("BNO: " + bno);
 	}
 }
